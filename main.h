75d308560bd6a5f662a10ae5a101b55248a59ac4
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-07-10 11:54:24 -0700

    main

--- main.h
+++ main.h
@@ -1,4 +1,41 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/* utils.c */
+int _strlen(const char *);
+int print(char *);
+char *itoa(long int, int);
+
+/* printf.c */
+int _printf(const char *, ...);
+
+/* handler.c */
+int handler(const char *, va_list);
+int percent_handler(const char *, va_list, int *);
+
+/* printers */
+int print_string(va_list);
+int print_char(va_list);
+
+/* _putchar.c */
+int _putchar(char);
+int buffer(char);
+
+/**
+ * struct _format - Typedef struct
+ * @type: Format
+ * @f: The function associated
+ */
+
+typedef struct _format
+
+{
+	char type;
+	int (*f)(va_list);
+} format;
+
 #endif
