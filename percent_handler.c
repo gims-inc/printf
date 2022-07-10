16fc4dbe809a61edc14b9a8df120f356a614b851
gims-inc <gabrielmusanga@gmail.com>
2022-07-10 15:44:46 -0700

    Pointer fmt

--- percent_handler.c
+++ percent_handler.c
@@ -12,12 +12,15 @@ int percent_handler(const char *str, va_list list, int *i)
 
 {
 	int size, j, number_formats;
+
 	format formats[] = {
 	{'s', print_string},{'c', print_char},
 	{'d', print_integer}, {'i', print_integer},
 	{'b', print_binary},{'X', print_hexadecimal_upp},
-	{'o', print_octal}, {'x', print_hexadecimal_low}
+	{'o', print_octal}, {'x', print_hexadecimal_low},
+	{'p', print_pointer}
 	};
+
 	*i = *i + 1;
 	if (str[*i] == '\0')
 	return (-1);
