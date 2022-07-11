e35c967c0432f3f19a2f20bef21082a735e8e218
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-07-11 02:08:31 -0700

    print pointer

--- print_pointer.c
+++ print_pointer.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
+int _strcmp(char *, char *);
 /**
  *print_pointer - Print a number in hexadecimal format
  *@list: Number to print
@@ -23,5 +24,22 @@ int print_pointer(va_list list)
 	size += print(p_buff);
 	return (size);
 }
+/**
+ * _strcmp - Compare two strings
+ * @s1: String 1
+ * @s2: String 2
+ * Return: Integer
+ */
 
+int _strcmp(char *s1, char *s2)
 
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		return (s1[i] - s2[i]);
+	}
+	return (0);
+}
